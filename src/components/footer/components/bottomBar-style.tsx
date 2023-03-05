3215d5261fbc4b3fa4472eacae469a8b62454136
gustavo.carvg <gustavo.carvg@gmail.com>
2023-03-05 18:09:20 -0300

    feat(footer/companies): adicionada a parte com as empresas responsáveis

--- src/components/footer/components/bottomBar-style.tsx
+++ src/components/footer/components/bottomBar-style.tsx
@@ -1,9 +1,38 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.section`
-    background: var(--color-2-500) ;
-    width: 100% ;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    height: 85px ;
-    padding-inline: 161px;
+  background: var(--color-2-500);
+  width: 100%;
+
+  height: 85px;
+  padding-inline: 161px;
+`
+
+export const ContainerSocialMedia = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 22px;
+`
+
+export const ContainerCompanies = styled.div`
+  display: flex;
+  gap: 20px;
+  color: var(--color-white);
+`
+export const ContentCompany = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5px;
+
+  span {
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 14px;
+  }
 `
--- src/components/footer/components/bottomBar.tsx
+++ src/components/footer/components/bottomBar.tsx
@@ -3,21 +3,38 @@ import iconInstagram from '../../../assets/icon-instagram.png'
 import iconLinkedin from '../../../assets/icon-linkedin.png'
 import iconYoutube from '../../../assets/icon-youtube.png'
 import paymentMethods from '../../../assets/paymentMethods.png'
+import logoMaeztra from '../../../assets/logoMaeztraFooter.png'
+import logoVtex from '../../../assets/logoVtex.png'
 
 import SocialMediaIcons from '../../UI/socialMediaIcons/socialMediaIcons'
 
-import { Container } from './bottomBar-style'
+import {
+  Container,
+  ContainerSocialMedia,
+  ContainerCompanies,
+  ContentCompany
+} from './bottomBar-style'
 
 const BottomBar = () => {
   return (
     <Container>
-      <div>
+      <ContainerSocialMedia>
         <SocialMediaIcons link="#" imageSrc={iconFacebook} />
         <SocialMediaIcons link="#" imageSrc={iconLinkedin} />
         <SocialMediaIcons link="#" imageSrc={iconInstagram} />
         <SocialMediaIcons link="#" imageSrc={iconYoutube} />
-      </div>
+      </ContainerSocialMedia>
       <img src={paymentMethods} alt="Métodos de pagamento" />
+      <ContainerCompanies>
+        <ContentCompany>
+          <span> Powered by</span>
+          <img src={logoVtex} />
+        </ContentCompany>
+        <ContentCompany>
+          <span> Developed by</span>
+          <img src={logoMaeztra} />
+        </ContentCompany>
+      </ContainerCompanies>
     </Container>
   )
 }
